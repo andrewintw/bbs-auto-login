5e62ab9cb33dc692568ed8870885207020897bb0
andrew.lin <andrew.lin@browan.com>
2019-08-06 21:04:22 +0800

    indent

--- ptt.sh
+++ ptt.sh
@@ -19,27 +19,27 @@
 #
 #    15,30,45 3 * * * LC_ALL="zh_TW.UTF-8" LANGUAGE="zh_TW.UTF-8" LANG="zh_TW.UTF-8" /path/to/ptt.sh
 
-set bbsID		"korea.fish"
-set bbsPW		"show-me-the-money"
+set bbsID      "korea.fish"
+set bbsPW      "show-me-the-money"
 
-set delayLong	"8"
-set delayShort	"2"
+set delayLong  "8"
+set delayShort "2"
 
 spawn ssh -oBatchMode=no -oStrictHostKeyChecking=no bbsu@ptt.cc
 
 expect {
-	"請輸入代號"						{ sleep $delayShort; send "$bbsID\r";	exp_continue }
-	"請輸入您的密碼"					{ sleep $delayShort; send "$bbsPW\r";	exp_continue }
-	"您想刪除其他重複登入的連線嗎"		{ sleep $delayShort; send "y\r"; exp_continue }
-#	"歡迎您再度拜訪"					{ sleep $delayShort; send "\r";  exp_continue }
-	"您要刪除以上錯誤嘗試的記錄嗎"		{ sleep $delayShort; send "y\r"; exp_continue }
-	"密碼不對或無此帳號"				{ sleep $delayShort; exit }
-	"請勿頻繁登入以免造成系統過度負荷"	{ sleep $delayShort; send "\r";  exp_continue }
-	"請按任意鍵繼續"					{ sleep $delayShort; send "\r";  exp_continue }
-	"按任意鍵繼續"						{ sleep $delayShort; send "\r";  exp_continue }
-#	"oodbye"							{ sleep $delayShort; interact }
-	"oodbye"							{ sleep $delayLong;  send "g\r"; exp_continue }
-	"您確定要離開"						{ sleep $delayShort; send "y\r"; exp_continue }
-	"此次停留時間"						{ sleep $delayShort; send "\r";  exp_continue }
+	"請輸入代號"                        { sleep $delayShort; send "$bbsID\r";	exp_continue }
+	"請輸入您的密碼"                    { sleep $delayShort; send "$bbsPW\r";	exp_continue }
+	"您想刪除其他重複登入的連線嗎"      { sleep $delayShort; send "y\r"; exp_continue }
+#	"歡迎您再度拜訪"                    { sleep $delayShort; send "\r";  exp_continue }
+	"您要刪除以上錯誤嘗試的記錄嗎"      { sleep $delayShort; send "y\r"; exp_continue }
+	"密碼不對或無此帳號"                { sleep $delayShort; exit }
+	"請勿頻繁登入以免造成系統過度負荷"  { sleep $delayShort; send "\r";  exp_continue }
+	"請按任意鍵繼續"                    { sleep $delayShort; send "\r";  exp_continue }
+	"按任意鍵繼續"                      { sleep $delayShort; send "\r";  exp_continue }
+#	"oodbye"                            { sleep $delayShort; interact }
+	"oodbye"                            { sleep $delayLong;  send "g\r"; exp_continue }
+	"您確定要離開"                      { sleep $delayShort; send "y\r"; exp_continue }
+	"此次停留時間"                      { sleep $delayShort; send "\r";  exp_continue }
 }
 exit
